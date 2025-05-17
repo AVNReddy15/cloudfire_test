95f2f14234c1016b8969065079aefd5f06d6207a
AVNReddy15 <vishnunarayanareddy88@gmail.com>
2025-05-17 16:06:35 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,2 +1,15 @@
 hello all
 welcome to world
+
+  // transfers your project from working directory
+// to staging area.
+git add .
+
+// transfers your project from staging area to 
+// Local Repository.
+git commit -m "your message here"
+
+
+// transfers project from local to central repository.
+// (requires internet)
+git push
